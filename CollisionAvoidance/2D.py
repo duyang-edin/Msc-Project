b146cc5543cc570d71a26b8bfb0c144e2209cff0
YANG DU <84846865+duyang-edin@users.noreply.github.com>
2021-09-14 19:52:57 +0100

    Update 2D.py

--- CollisionAvoidance/2D.py
+++ CollisionAvoidance/2D.py
@@ -3,15 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import MultipleLocator
 
-# builds a N sided polygon approximation of a circle for MIP. It is the union of the segments making up the polygon
-# might also be useful to directly encode arcs. for joint constraint limits.
-
+# builds a N sided polygon approximation of a circle for MIP
 def circle(N):
+
     x = cvx.Variable()
     y = cvx.Variable()
     l = cvx.Variable(N)
 
-    segment = cvx.Variable(N,boolean=True) #segment indicator variables, relaxing the boolean constraint gives the convex hull of the polygon
+    segment = cvx.Variable(N,boolean=True) #segment indicator variables
 
     angles = np.linspace(0, 2*np.pi, N, endpoint=False)  #divide 2pi
     xs = np.cos(angles)
@@ -20,26 +19,26 @@ def circle(N):
     constraints = []
     constraints = [x == l*xs, y == l*ys]
     constraints = constraints + [cvx.sum(l) == 1, l <= 1, 0 <= l]
-    constraints = constraints +  [cvx.sum(segment) == 1] # only one indicator variable can be nonzero
+    constraints = constraints +  [cvx.sum(segment) == 1] # only one variable can be nonzero
 
-    constraints = constraints +  [l[N-1] <= segment[N-1] + segment[0]] #special wrap around case
+    constraints = constraints +  [l[N-1] <= segment[N-1] + segment[0]]
     for i in range(N-1):
-        constraints = constraints + [l[i] <= segment[i] + segment[i+1]] # interpolation variables suppressed
+        constraints = constraints + [l[i] <= segment[i] + segment[i+1]]
     return x, y, constraints
 
 def main():
     # build a 2d rotation matrix using circle
     def R(N):
         constraints = []
-        c, s, constraint = circle(N) # get cosines and sines from a circle
-        constraints += constraint
-        r = cvx.Variable((2,2)) # build rotation matrix
-        constraints += [r[0,0] == c, r[0,1] == s]
-        constraints += [r[1,0] == -s, r[1,1] == c]
-        return r, constraints
+        c, s, constraint = circle(N);
+        constraints += constraint;
+        r = cvx.Variable((2,2)); # rotation matrix
+        constraints += [r[0,0] == c, r[0,1] == s];
+        constraints += [r[1,0] == -s, r[1,1] == c];
+        return r, constraints;
         # np.array([[c , s], [-s, c]])
 
-    link_lengths = [1,1,1,1]
+    link_lengths = [1,1,1,1]; # length of each link
     pivots = []
     Rs = []
     N = len(link_lengths)
@@ -60,25 +59,24 @@ def main():
 
         constraints = constraints + [p2 == p1 + R1 * np.array([l,0]) ];
 
-
+        # the midpoint of each link will avoid an obstacle
         mid = (p1 + p2)/2;
         Setvalue=1000;
-        iii = cvx.Variable(N,boolean=True);
-        constraints = constraints + [mid[0] <= 0.7 + iii[0] * Setvalue];
-        constraints = constraints + [mid[0] + iii[1] * Setvalue >= 1.1 ];
-        constraints = constraints + [mid[1] + iii[2] * Setvalue >= 1.1 ];
-        constraints = constraints + [mid[1] <= 0.7 + iii[3] * Setvalue ];
-        constraints = constraints + [cvx.sum(iii) <= N-1, iii <= 1, iii >= 0];
+        i = cvx.Variable(N,boolean=True);
+        constraints = constraints + [mid[0] <= 0.7 + i[0] * Setvalue];
+        constraints = constraints + [mid[0] + i[1] * Setvalue >= 1.1 ];
+        constraints = constraints + [mid[1] + i[2] * Setvalue >= 1.1 ];
+        constraints = constraints + [mid[1] <= 0.7 + i[3] * Setvalue ];
+        constraints = constraints + [cvx.sum(i) <= N-1, i <= 1, i >= 0];
 
+        # the midpoint of each link will avoid an obstacle
         random = (p1 + p2)/Len;
-        Setvalue=1000;
         iii = cvx.Variable(N,boolean=True);
         constraints = constraints + [random[0] <= 0.7 + iii[0] * Setvalue];
         constraints = constraints + [random[0] + iii[1] * Setvalue >= 1.1 ];
         constraints = constraints + [random[1] + iii[2] * Setvalue >= 1.1 ];
         constraints = constraints + [random[1] <= 0.7 + iii[3] * Setvalue ];
         constraints = constraints + [cvx.sum(iii) <= N-1, iii <= 1, iii >= 0];
-
 
         p1 = p2;
 
@@ -92,7 +90,9 @@ def main():
         pivots.append(p2)
         Rs.append(R1)
 
+    # Here is the end-effector position
     end_position = np.array([2,2])
+
     constraints = constraints + [p2 == end_position]
 
     objective = cvx.Maximize(1)
@@ -113,13 +113,18 @@ def main():
     ax = plt.gca()
     ax.xaxis.set_major_locator(x_major_locator)
     ax.yaxis.set_major_locator(y_major_locator)
+
+    # set the view range
     plt.xlim(-1, 3)
     plt.ylim(-0.5, 3)
+
+    # an obstacle consists of four points:[0.7,0.7],[0.7,1.1],[1.1,1.1],[1.1,0.7]
     plt.plot([0.7,0.7],[0.7,1.1],'b-');
     plt.plot([0.7,1.1],[1.1,1.1],'b-');
     plt.plot([1.1,1.1],[1.1,0.7],'b-');
     plt.plot([1.1,0.7],[0.7,0.7],'b-');
 
+    # additional text information in image
     plt.text(1.1, 0.85, 'Obstacle')
     plt.text(0, 0, 'Origin')
     plt.text(0, 0, 'Origin')
